37fc8c2eefb404bf13c787c946ca52c3da925ecc
JC#21 <canulo1234@gmail.com>
2026-01-15 12:35:08 +0800

    first

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $user_data=[
+            [
+                'fname' => "Jhon Carlo",
+                'mname' => "Sabuero",
+                'lname' => "Canulo",
+                'gender' => "male",
+                'role' => "admin",
+                'email' => "canulodev21@gmail.com",
+                'password' => Hash::make('canulo2121'),
+            ],
+        ];
+        User::insert($user_data);
+    }
+}
